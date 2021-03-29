bab5583928b848d0fa122c02a2e5bcdfad23bb6c
HuCutie <husmallcutie@gmail.com>
2021-03-29 15:50:52 +0800

    2021/03/29 Tired

--- 35.搜索插入位置.cpp
+++ 35.搜索插入位置.cpp
@@ -8,7 +8,26 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-
+        if(target < nums[0])
+        {
+            nums.insert(nums.begin(), target);
+            return 0;
+        }   
+        if(target > nums[nums.size() - 1])
+        {
+            nums.push_back(target);
+            return nums.size() - 1;
+        }
+        vector<int>::iterator it;
+        int i;
+        for(i = 0, it = nums.begin(); i < nums.size() - 1; i++, it++)
+        {
+            if(*it < target && target < *it)
+            {
+                nums.insert(it + 1, target);
+                return i;
+            }
+        }       
     }
 };
 // @lc code=end
