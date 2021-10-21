35104c213d177c43967c8eba953665aafd45dfce
HuCutie <husmallcutie@gmail.com>
2021-10-21 13:45:29 +0800

    T203 removeElements

---
+++ 203.移除链表元素.cpp
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=cpp
+ *
+ * [203] 移除链表元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(head == NULL)
+        {
+            return head;
+        }
+
+        ListNode * HEAD = new ListNode(0, head);
+        ListNode * p = HEAD;
+        while(p->next != NULL)
+        {
+            if(p->next->val == val)
+            {
+                p->next = p->next->next;
+            }
+            else
+            {
+                p = p->next;
+            }
+        }
+
+        return HEAD->next;
+    }
+};
+// @lc code=end
+
